4e2416199655da3c21f567e538a3b3ca65f603d2
Ken Van den Enden <ken@enden.be>
2024-11-26 14:25:16 +0100

    Index market resolution events.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,6 @@
 import { ponder } from "@/generated";
 import { marketAbi } from "../abis/flipside";
-import { markets, pools } from "../ponder.schema";
+import { markets, outcomes, pools } from "../ponder.schema";
 
 ponder.on("MarketFactory:MarketCreated", async ({ event, context }) => {
   const { market, creator, collateralToken, pool, initialLiquidity } = event.args;
@@ -63,71 +63,12 @@ ponder.on("MarketFactory:MarketCreated", async ({ event, context }) => {
   });
 });
 
-// ponder.on("MarketFactory:MarketCreated", async ({ event, context }) => {
-//   const { market, creator, collateralToken, pool, initialLiquidity } = event.args;
-//   const { client, db } = context;
+ponder.on("Resolver:MarketResolved", async ({ event, context }) => {
+  const { market, outcome } = event.args;
+  const { db } = context;
 
-//   const unitPrice = await client.readContract({
-//     abi: marketAbi,
-//     address: market,
-//     functionName: "unitPrice",
-//   });
-
-//   const title = await client.readContract({
-//     abi: marketAbi,
-//     address: market,
-//     functionName: "title",
-//   });
-
-//   const description = await client.readContract({
-//     abi: marketAbi,
-//     address: market,
-//     functionName: "description",
-//   });
-
-//   const expirationDate = await client
-//     .readContract({
-//       abi: marketAbi,
-//       address: market,
-//       functionName: "expirationDate",
-//     })
-//     .then(Number);
-
-//   const longToken = await client.readContract({
-//     abi: marketAbi,
-//     address: market,
-//     functionName: "longToken",
-//   });
-
-//   const shortToken = await client.readContract({
-//     abi: marketAbi,
-//     address: market,
-//     functionName: "shortToken",
-//   });
-
-//   await db.Market.create({
-//     id: market,
-//     data: {
-//       creator,
-//       collateralToken,
-//       unitPrice,
-//       title,
-//       description,
-//       expirationDate,
-//       longToken,
-//       shortToken,
-//       resolved: false,
-//       outcome: undefined,
-
-//       createdAt: Number(event.block.timestamp),
-//     },
-//   });
-
-//   await db.Pool.create({
-//     id: pool,
-//     data: {
-//       initialLiquidity,
-//       marketId: market,
-//     },
-//   });
-// });
+  await db.update(markets, { id: market }).set({
+    resolved: true,
+    outcome: outcomes.enumValues[outcome] ?? "Invalid",
+  });
+});
