dadd9da38de504984b09cd3c4677fee463514f48
Ken Van den Enden <ken@enden.be>
2024-12-14 20:30:36 +0100

    Redeem single outcome.

--- contracts/src/Flipside.sol
+++ contracts/src/Flipside.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { IV3SwapRouter } from "./interfaces/uniswap/IV3SwapRouter.sol";
@@ -13,6 +14,8 @@ import { Market } from "./Market.sol";
 import { MarketFactory } from "./MarketFactory.sol";
 
 contract Flipside {
+  using SafeERC20 for IERC20;
+
   MarketFactory public marketFactory;
   IV3SwapRouter public swapRouter;
 
@@ -25,8 +28,8 @@ contract Flipside {
     IERC20 collateralToken = IERC20(params.collateralToken);
     uint256 collateralAmount = Price.calculate(params.initialLiquidity, params.unitPrice);
 
-    collateralToken.transferFrom(msg.sender, address(this), collateralAmount);
-    collateralToken.approve(address(marketFactory), collateralAmount);
+    collateralToken.safeTransferFrom(msg.sender, address(this), collateralAmount);
+    collateralToken.safeIncreaseAllowance(address(marketFactory), collateralAmount);
 
     return marketFactory.createMarket(params);
   }
@@ -37,32 +40,48 @@ contract Flipside {
 
   function mintOutcome(IMarket market, address to, uint256 amount, uint256 amountOutMin, bool outcome) external {
     _mint(market, address(this), amount);
-    address longToken = address(market.longToken());
-    address shortToken = address(market.shortToken());
+    IERC20 longToken = market.longToken();
+    IERC20 shortToken = market.shortToken();
 
-    (address tokenIn, address tokenOut) = outcome ? (shortToken, longToken) : (longToken, shortToken);
-    uint256 amountOut = _swap(tokenIn, tokenOut, amount, amountOutMin);
+    (IERC20 tokenIn, IERC20 tokenOut) = outcome ? (shortToken, longToken) : (longToken, shortToken);
+    uint256 amountOut = _swapExactIn(tokenIn, tokenOut, amount, amountOutMin);
 
-    IERC20(tokenOut).transfer(to, amount + amountOut);
+    tokenOut.safeTransfer(to, amount + amountOut);
+  }
+
+  function redeemOutcome(IMarket market, address to, uint256 amount, uint256 amountInMax, bool outcome) external {
+    IERC20 longToken = market.longToken();
+    IERC20 shortToken = market.shortToken();
+
+    (IERC20 tokenIn, IERC20 tokenOut) = outcome ? (longToken, shortToken) : (shortToken, longToken);
+
+    tokenIn.safeTransferFrom(msg.sender, address(this), amount + amountInMax);
+    uint256 amountIn = _swapExactOut(tokenIn, tokenOut, amountInMax, amount);
+
+    market.redeem(to, amount);
+
+    if (amountIn < amountInMax) {
+      tokenIn.safeTransfer(msg.sender, amountInMax - amountIn);
+    }
   }
 
   function _mint(IMarket market, address to, uint256 amount) internal {
     IERC20 collateralToken = market.collateralToken();
     uint256 collateralAmount = Price.calculate(amount, market.unitPrice());
-    collateralToken.transferFrom(msg.sender, address(this), collateralAmount);
-    collateralToken.approve(address(market), collateralAmount);
+    collateralToken.safeTransferFrom(msg.sender, address(this), collateralAmount);
+    collateralToken.safeIncreaseAllowance(address(market), collateralAmount);
     market.mint(to, amount);
   }
 
-  function _swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOutMin)
+  function _swapExactIn(IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn, uint256 amountOutMin)
     internal
     returns (uint256 amountOut)
   {
-    IERC20(tokenIn).approve(address(swapRouter), amountIn);
+    tokenIn.safeIncreaseAllowance(address(swapRouter), amountIn);
 
     IV3SwapRouter.ExactInputSingleParams memory params = IV3SwapRouter.ExactInputSingleParams({
-      tokenIn: tokenIn,
-      tokenOut: tokenOut,
+      tokenIn: address(tokenIn),
+      tokenOut: address(tokenOut),
       fee: 10_000,
       recipient: address(this),
       amountIn: amountIn,
@@ -71,5 +90,24 @@ contract Flipside {
     });
 
     amountOut = swapRouter.exactInputSingle(params);
+  }
+
+  function _swapExactOut(IERC20 tokenIn, IERC20 tokenOut, uint256 amountInMax, uint256 amountOut)
+    internal
+    returns (uint256 amountIn)
+  {
+    tokenIn.safeIncreaseAllowance(address(swapRouter), amountInMax);
+
+    IV3SwapRouter.ExactOutputSingleParams memory params = IV3SwapRouter.ExactOutputSingleParams({
+      tokenIn: address(tokenIn),
+      tokenOut: address(tokenOut),
+      fee: 10_000,
+      recipient: address(this),
+      amountInMaximum: amountInMax,
+      amountOut: amountOut,
+      sqrtPriceLimitX96: 0
+    });
+
+    amountIn = swapRouter.exactOutputSingle(params);
   }
 }
